c269180e678ec921070251274f04a2ce6befb8ee
JaimeAndres03 <jaime.quinones.2021@upb.edu.co>
2021-03-23 18:34:41 -0500

    Ejercicio_3_Dijitos_5_al_20_3_en_3

--- dijitos_5_y_20_entre_3.py
+++ dijitos_5_y_20_entre_3.py
@@ -1,5 +1,7 @@
+print("*EJERCICIO 3* - Dijitos_5_al_20_3_en_3")
 check = "✓"
 for n in range(5, 21, 3):
-    print("N° ", n, check)
-#Ejercicio #3
+    print("N° {0} {1}".format(n, check))
+#Otra forma de hacerlo...
+print("EJERCICIO 3")
 print(list(range(5, 21, 3)))
