85f7bd04c94a3303b6337ce946b130f72368e347
JaimeAndres03 <jaime.quinones.2021@upb.edu.co>
2021-03-23 14:05:17 -0500

    Ejercicio_1_Dijitos_0_al_9

---
+++ Dijitos_0_al_9.py
@@ -0,0 +1,7 @@
+print("*EJERCICIO 1* - Dijitos_0_al_9")
+check = "✓"
+for x in range(10):
+    print("N° {0} {1}".format(x, check))
+#Otra forma de hacerlo..
+print("EJERCICIO 1")
+print(list(range(0, 10)))
