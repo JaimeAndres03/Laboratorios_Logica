c1a0b961a28a6319b666eee0cd93bf657841f079
JaimeAndres03 <jaime.quinones.2021@upb.edu.co>
2021-03-23 18:15:42 -0500

    Ejercicio_2_Dijitos_100_al_199

---
+++ Dijitos_100_al_199.py
@@ -0,0 +1,7 @@
+print("*EJERCICIO 2* - Dijitos_100_al_199")
+check = "✓"
+for x in range(100, 200):
+    print("N° {0} {1}".format(x, check))
+#Otra forma de hacerlo...
+print("EJERCICIO 2")
+print(list(range(100, 200)))
